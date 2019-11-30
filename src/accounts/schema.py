d5255524223145006da59506aba9a9c0dfd349f4
Yanis Bouchenna <bouchenna.yanis@protonmail.com>
2019-11-30 23:25:52 +0100

    Add user registration mutation to accounts schema

--- src/accounts/schema.py
+++ src/accounts/schema.py
@@ -15,10 +15,25 @@ class Query(graphene.ObjectType):
     def resolve_users(self, info, **kwargs):
         return get_user_model().objects.all()
 
+class RegisterNewUserMutation(graphene.Mutation):
+    class Arguments:
+        email = graphene.String(required=True)
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+    
+    new_user = graphene.Field(UserType)
+
+    def mutate(self, info, email, username, password):
+        user_model = get_user_model()
+        new_user = user_model(email=email, user_name=username)
+        new_user.set_password(password)
+
+        new_user.save()
+        return RegisterNewUserMutation(new_user=new_user)
+
 class Mutation(graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
 
-    def resolve_users(self, info, **kwargs):
-        return get_user_model().objects.all()
+    register_new_user = RegisterNewUserMutation.Field()
