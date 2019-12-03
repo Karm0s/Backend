de2089ddee7c4ae7c33b1f66b0eac014fd399f96
Yanis Bouchenna <bouchenna.yanis@protonmail.com>
2019-12-03 16:18:20 +0100

    Create Anime, Episode models

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from modelchoices import Choices
 
-# Create your models here.
+
+class AnimeStatus(Choices):
+    FINISHED = ('F', 'Finished')
+    CURRENTLY = ('C', 'Currently')
+
+class Anime(models.Model):
+    
+    title = models.CharField(max_length=150)
+    status = models.CharField(max_length=1, choices=AnimeStatus.CHOICES)
+
+class Episode(models.Model):
+    number = models.IntegerField()
+    volume = models.IntegerField()
+    from_chapter = models.IntegerField()
+    to_chapter = models.IntegerField()
+
+    anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
+
+
